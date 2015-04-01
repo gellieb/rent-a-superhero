9ea59a1ebf3c75468023a738945523a5720dd615
Angel Baek <angelbaek1@gmail.com>
2015-04-01 15:10:43 -0400

    create edit route for user's rentals

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -4,9 +4,27 @@ get '/user/:id/index' do
 end
 
 get '/user/:id/edit' do
-  erb :'user/edit'
+  user = User.find_by(id: params[:id])
+  erb :'user/edit', locals: {user: user, rentals: user.rentals}
+end
+
+put '/user/:id/edit' do
+  user = User.find_by(id: params[:id])
+  params[:reason].keys.each do |id|
+    rented_hero = user.rentals.find_by(superhero: id)
+    rented_hero.update_attributes(reason: params[:reason][id])
+  end
+
+  redirect "/user/#{current_user.id}/index"
+end
+
+
+get '/user/:id/delete' do
+
+
 end
 
 delete '/user/:id/delete' do
+  redirect "/user/#{current_user.id}/index"
 
 end
